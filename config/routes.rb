460378f9d32e8599e6586d7d95fbbf683d039ab7
EC2 Default User <ec2-user@ip-172-31-92-181.ec2.internal>
2022-04-30 06:50:44 +0000

    [add]実装済：投稿機能（new/create）、投稿詳細画面(show)、投稿編集画面(edit/update)、削除機能(destroy)。未実装：サクセスメッセージ・エラーメッセージ・装飾(CSS)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
-  # resouces :books　⇦後で実行
+  # resouces :books 後で実行
   get 'books' => 'books#new'
-  get 'books/show'
-  get 'books/edit'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
 
 
 end
